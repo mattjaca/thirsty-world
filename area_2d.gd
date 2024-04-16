b34b17b5bc4c0a2394dde8025416c60efe22a57a
gavinator9 <epikdude@rocketmail.com>
2024-04-15 17:17:12 -0700

    Added basic player functionality for overworld

---
+++ area_2d.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var speed = 400 # how fast the player moves pixel/sec
+var screen_size #size of game window
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
