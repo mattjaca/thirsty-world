b1397de35879736345e2a91641ab4b7ff9f16440
gavinator9 <epikdude@rocketmail.com>
2024-04-15 17:38:14 -0700

    main menu button logic tweaks

--- Button.gd
+++ Button.gd
@@ -5,11 +5,15 @@ extends Node2D
 @onready var playNode = $CanvasLayer/Control/TextureRect/Start
 @onready var playAnim = $CanvasLayer/AnimationPlayer
 @onready var Quit = $CanvasLayer/Control/TextureRect/Quit
+@onready var Credits = $CanvasLayer/Control/TextureRect/Credits
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Quit.connect("button_down", get_tree().quit)
 	playNode.connect("button_down", func(): 
 		playAnim.play("revolvercylinder")
+		playNode.hide()
+		Credits.hide()
+		Quit.hide()
 		#get_tree().quit()
 	) ## Object.function(signalName, callable)
 
